635a23d50cb1b2e152a1eb4de92f7b0251078be8
niranjan <niranjanamurthy@gmail.com>
2017-07-24 19:34:54 +1000

    added add and sub funtions

--- project_main.c
+++ project_main.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include "headers.h"
+#include "functions.c"
 
 void display()
 {
@@ -7,6 +9,9 @@ void display()
 
 int main()
 {
+  int x = 20,y = 50;
   printf("Welcome to cproj project\n");
   display();
+  printf("Adiition of %d and %d is %d\n",x,y,add(x,y));
+  printf("Subtraction of %d and %d is %d\n",x,y,sub(x,y));
 }
